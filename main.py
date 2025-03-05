5747ecbc5bf9163b603638a6c9685e026dbb62b6
iainrt <iainrt@gmail.com>
2025-03-05 21:32:49 +0000

    added classes for Player and CircleShape, added player to screen

--- main.py
+++ main.py
@@ -3,20 +3,26 @@
 # throughout this file
 import pygame
 from constants import *
+import player
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    player_instance = player.Player(x = SCREEN_WIDTH/2, y = SCREEN_HEIGHT/2)
+    dt = 0
+    
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        
         screen.fill(color = "black")
+        player_instance.draw(screen)
         pygame.display.flip()
-
-    print("Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+        
+        # limit framerate to 60 FPS
+        dt = clock.tick(60)/1000
 
 if __name__ == "__main__":
     main()
